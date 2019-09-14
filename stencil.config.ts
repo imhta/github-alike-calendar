d9fb582027cb90ec41385b6e7e8306eb43d08caa
imhta <imhtapm@gmail.com>
2019-09-14 13:41:20 +0530

    Initial commit

---
+++ stencil.config.ts
@@ -0,0 +1,18 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'github-alike-calender',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader'
+    },
+    {
+      type: 'docs-readme'
+    },
+    {
+      type: 'www',
+      serviceWorker: null // disable service workers
+    }
+  ]
+};
